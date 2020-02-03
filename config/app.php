797729d4a763b7319637ec8b55c1cf6dea7ceb3f
kuzugr <monmon.syusyo@gmail.com>
2020-02-03 22:29:05 +0900

    ローカルのデータベースの設定を追加

--- config/app.php
+++ config/app.php
@@ -274,6 +274,10 @@ return [
             'driver' => Mysql::class,
             'persistent' => false,
             'timezone' => 'UTC',
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'samplecake',
 
             /**
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
