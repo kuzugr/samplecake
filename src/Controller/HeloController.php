5e4292b086947de0da8bf3f4db4297e455b736b9
kuzugr <monmon.syusyo@gmail.com>
2020-02-08 21:23:01 +0900

    レイアウトテンプレートの追加

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -5,6 +5,14 @@ use App\Controller\AppController;
 
 class HeloController extends AppController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->viewBuilder()->setLayout('sample');
+        $this->set('header', '* this is sample site *');
+        $this->set('footer', 'copyright 2015 libro.');
+    }
+
     public function index()
     {
         $str = $this->request->getData('text1');
---
+++ templates/layout/sample.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title><?= h($this->fetch('title')) ?></title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <?php
+        echo $this->Html->css('sample.css');
+        echo $this->fetch('meta');
+        echo $this->fetch('css');
+        echo $this->fetch('script');
+        ?>
+    </head>
+    <body>
+    <?php
+    ?>
+        <div id="header"><?= $header ?></div>
+
+        <?= $this->fetch('content') ?>
+
+        <div id="footer"><?= $footer ?></div>
+    </body>
+</html>
